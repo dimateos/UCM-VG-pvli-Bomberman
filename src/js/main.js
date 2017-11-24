e141bcf823930a19b01f21d0272d23e8f58c96a9
Diego Mateos <dimateos@ucm.es>
2017-11-24 14:21:20 +0100

    Timestamps for states, little touches html, sprites pack

--- src/js/main.js
+++ src/js/main.js
@@ -2,31 +2,42 @@
 
 var PlayScene = require('./play_scene.js');
 
-
 var BootScene = {
+
   preload: function () {
+    this.startTime = Date.now(); //to calculate booting time
+
     // load here assets required for the loading screen
-    this.game.load.image('preloader_bar', 'images/preloader_bar.png');
-  },
+    this.game.load.image('preloader_logo', 'images/phaser.png');
+},
 
   create: function () {
+
+    //this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    //this.scale.pageAlignHorizontally = true;
+    //this.scale.setScreenSize();
+
     this.game.state.start('preloader');
+    console.log("Booting...", Date.now()-this.startTime, "ms");
   }
 };
 
 
 var PreloaderScene = {
   preload: function () {
-    this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
+    this.startTime = Date.now(); //to calculate booting time
+
+    this.loadingBar = this.game.add.sprite(0, 0, 'preloader_logo');
     this.loadingBar.anchor.setTo(0, 0.5);
     this.load.setPreloadSprite(this.loadingBar);
 
     // TODO: load here the assets for the game
-    this.game.load.image('logo', 'images/phaser.png');
+    this.game.load.image('logo', 'images/readme/portada.jpg');
   },
 
   create: function () {
     this.game.state.start('play');
+    console.log("Preloading...", Date.now()-this.startTime, "ms");
   }
 };
 
--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -1,10 +1,19 @@
 'use strict';
 
 var PlayScene = {
+  preload: function () {
+    this.startTime = Date.now(); //to calculate booting time
+  },
+
   create: function () {
+
     var logo = this.game.add.sprite(
       this.game.world.centerX, this.game.world.centerY, 'logo');
+
     logo.anchor.setTo(0.5, 0.5);
+    logo.scale.setTo(0.8,0.85); // to fit in the canvas
+
+    console.log("Loaded...", Date.now()-this.startTime, "ms");
   }
 };
 
